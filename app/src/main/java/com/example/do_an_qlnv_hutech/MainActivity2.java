74b18a3bac4b0901590fd4c9d592ce2d1e4c69e8
Danh IT <danhzxc555@gmail.com>
2024-12-09 10:41:16 +0700

    10h41 - 9/12/24

--- app/src/main/java/com/example/do_an_qlnv_hutech/MainActivity2.java
+++ app/src/main/java/com/example/do_an_qlnv_hutech/MainActivity2.java
@@ -1,9 +1,6 @@
 package com.example.do_an_qlnv_hutech;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,17 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.sql.Connection;
+public class MainActivity2 extends AppCompatActivity {
 
-public class RegisterActivity extends AppCompatActivity {
-    EditText edtdangky, edtpassword, conformpassword;
-    Button btnregister; // Nút đăng ký
-    Connection conn; // Kết nối cơ sở dữ liệu
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
-        
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
